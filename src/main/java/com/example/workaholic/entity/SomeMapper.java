9c62f5294971d2a1d964d2c83176b3729779f213
rohitkurbetti <69436380+rohitkurbetti@users.noreply.github.com>
2023-07-29 04:44:19 +0530

    Update SomeMapper.java

--- src/main/java/com/example/workaholic/entity/SomeMapper.java
+++ src/main/java/com/example/workaholic/entity/SomeMapper.java
@@ -6,11 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 public class SomeMapper {
 	private String assignment;
 	private String branch;
